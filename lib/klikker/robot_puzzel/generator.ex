a53957b7ed72a08d33db3b4371180e8a86f48726
Arnold van der Meulen <arnold@roqua.nl>
2018-02-21 00:12:02 +0100

    Make generator functions private. Add some docs.

--- lib/klikker/robot_puzzel/generator.ex
+++ lib/klikker/robot_puzzel/generator.ex
@@ -1,4 +1,6 @@
 defmodule Klikker.RobotPuzzel.Generator do
+  @moduledoc false
+
   @no_rows 5
   @no_cols 5
   @no_cells @no_rows * @no_cols
@@ -9,35 +11,47 @@ defmodule Klikker.RobotPuzzel.Generator do
   @max_moves 10
   @min_moves 4
 
+  @doc """
+  Generate a new lunar lander puzzle.
+  """
   def generate do
     generate_puzzel()
-    |> IO.inspect
+    # |> IO.inspect
     |> assign_colors()
     |> Enum.into([], fn {c, i} -> %{color: c, index: i} end)
   end
 
-  def assign_colors({_, position, red_index, _}) do
+  defp assign_colors({_, position, red_index, _}) do
     @colors
     |> List.insert_at(red_index, "red")
     |> Enum.zip(position)
   end
 
-  def generate_puzzel do
+  defp generate_puzzel do
     solve(place_robots(), @base_idx)
     |> List.wrap
     |> Enum.filter(fn result -> elem(result, 0) == :has_solution end)
     |> case do
       [] ->
+        # No solution found for this start position, try again
         generate_puzzel()
       positions ->
         case find_best_position(positions) do
+          # Start position is solved in too easily, try again
           {_, _, _, moves} when moves < @min_moves -> generate_puzzel()
+          # This is a valid start position
           position -> position
         end
     end
   end
 
-  def find_best_position(positions) do
+  # Return the indexes where robots are placed
+  defp place_robots do
+    Enum.take_random(0..@no_cells - 1, 2 + :rand.uniform(4))
+  end
+
+  # Find start position for red where it has the greatest minimal no_moves
+  defp find_best_position(positions) do
     positions
     |> Enum.sort_by(fn {_, _, _, no_moves} -> no_moves end)
     |> Enum.group_by(fn {_, _, red_index, _} -> red_index end)
@@ -47,12 +61,13 @@ defmodule Klikker.RobotPuzzel.Generator do
     |> List.last
   end
 
-  def solve(robots, base_idx) do
+  # Try to solve the puzzle when starting with robots (list of indices)
+  defp solve(robots, base_idx) do
     solve(robots, base_idx, robots, 1)
   end
 
-  def solve(_, _, start_robots, @max_moves), do: {:max_depth_reached, start_robots}
-  def solve(robots, base_idx, start_robots, depth) do
+  defp solve(_, _, start_robots, @max_moves), do: {:max_depth_reached, start_robots}
+  defp solve(robots, base_idx, start_robots, depth) do
     # For comparison purposes
     empty_result = List.duplicate([], length(robots))
 
@@ -80,7 +95,7 @@ defmodule Klikker.RobotPuzzel.Generator do
     end
   end
 
-  def perform_moves(robots, spots) do
+  defp perform_moves(robots, spots) do
     spots
     |> Enum.with_index
     |> Enum.flat_map(fn {spots2, idx} ->
@@ -91,14 +106,14 @@ defmodule Klikker.RobotPuzzel.Generator do
     end)
   end
 
-  def find_target_spots(robots, from_idx) do
+  defp find_target_spots(robots, from_idx) do
     look_left(robots, from_idx) ++
     look_right(robots, from_idx) ++
     look_up(robots, from_idx) ++
     look_down(robots, from_idx)
   end
 
-  def look_left(robots, from_idx) do
+  defp look_left(robots, from_idx) do
     from_col = rem(from_idx, @no_cols)
     cond do
       from_col - 1 < 0  ->
@@ -110,7 +125,7 @@ defmodule Klikker.RobotPuzzel.Generator do
     end
   end
 
-  def look_right(robots, from_idx) do
+  defp look_right(robots, from_idx) do
     from_col = rem(from_idx, @no_cols)
     cond do
       from_col + 1 >= @no_cols  ->
@@ -122,7 +137,7 @@ defmodule Klikker.RobotPuzzel.Generator do
     end
   end
 
-  def look_up(robots, from_idx) do
+  defp look_up(robots, from_idx) do
     from_row = div(from_idx, @no_cols)
     cond do
       from_row - 1 < 0  ->
@@ -134,7 +149,7 @@ defmodule Klikker.RobotPuzzel.Generator do
     end
   end
 
-  def look_down(robots, from_idx) do
+  defp look_down(robots, from_idx) do
     from_row = div(from_idx, @no_cols)
     cond do
       from_row + 1 >= @no_rows  ->
@@ -145,12 +160,4 @@ defmodule Klikker.RobotPuzzel.Generator do
         look_down(robots, from_idx + @no_cols)
     end
   end
-
-  @doc """
-  Return the indexes where robots are placed
-  """
-  def place_robots do
-    Enum.take_random(0..@no_cells - 1, 2 + :rand.uniform(4))
-  end
-
 end
